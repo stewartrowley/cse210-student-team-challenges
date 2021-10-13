651d9b7fa684eda879ad910db3c19cb0b8df4f19
Kaden Spencer <kaspen24@gmail.com>
2021-10-13 10:42:09 -0600

    updates

--- 05-jumper/jumper/game/console.py
+++ 05-jumper/jumper/game/console.py
@@ -5,10 +5,6 @@ class Console:
     def read(self, prompt):
 
         return input(prompt)
-
-    def read_number(self, prompt):
-     
-        return float(input(prompt))
         
     def write(self, text):
   
