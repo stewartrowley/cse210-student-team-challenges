c5429813e1b167ff09b14a5a6d41ee1e2558e40d
garrettbadger <garrett.badger.id@gmail.com>
2021-10-13 11:11:28 -0600

    modified the jumper class

--- 05-jumper/jumper/game/jumper.py
+++ 05-jumper/jumper/game/jumper.py
@@ -1,4 +1,18 @@
 
 class Jumper:
 
-def __init__(self):
+    def __init__(self):
+        self.guess = []
+
+    def check_guess(self, word):
+        correct = False
+        for element in word:
+            if self.guess[-1] == element:
+                correct = True
+        for element in self.guess:
+            if self.guess[-1] == element:
+                correct = False
+        return correct
+            
+
+
