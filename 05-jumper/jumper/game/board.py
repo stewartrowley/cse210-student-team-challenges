f86dc6244a36903398a2a4669ce241c8760d51ef
Row84M <stewy.rowley@gmail.com>
2021-10-15 11:15:15 -0600

    update

--- 05-jumper/jumper/game/board.py
+++ 05-jumper/jumper/game/board.py
@@ -1,19 +1,50 @@
-class board:
-    def display_word():
-        letter = []
-        for letter in len(word):
-            display += '_'
-            print(display)
-        for position in range(len(word)):
-            letter = word[position]
-            if guess == True:
-                display[position] = letter
-            else:
-                print(display)
+class Board:
+    def __init__(self):
+        self.word = ''
+        self.display = []
 
-    def display_jumper():
-        if guess == false:
-            parachute_display = ''
-        elif
+    def display_word(self, guess):
+        letter = []
+        for letter in len(self.word):
+            self.display.append('_')
+            print(self.display)
+        for position in range(len(self.word)):
+            letter = self.word[position]
+            if guess == letter:
+                self.display[position] = letter
+                print(self.display)
+
+    def display_jumper(self, wrong):
+        if wrong == 0:
+            print(' ' + ' ' + '___')
+            print(' /' + '___' + '\\')
+            print(' ' + '\\' + '   ' + '/')
+            print('  ' + '\\' + ' ' + '/')
+            print('   0')
+            print('  /|\\')
+            print('  /' + ' ' + '\\')
+        elif wrong >= 1:
+            print(' /' + '___' + '\\')
+            print(' ' + '\\' + '   ' + '/')
+            print('  ' + '\\' + ' ' + '/')
+            print('   0')
+            print('  /|\\')
+            print('  /' + ' ' + '\\')
+        elif wrong >= 2:
+            print(' ' + '\\' + '   ' + '/')
+            print('  ' + '\\' + ' ' + '/')
+            print('   0')
+            print('  /|\\')
+            print('  /' + ' ' + '\\')
+        elif wrong >= 3:
+            print('  ' + '\\' + ' ' + '/')
+            print('   0')
+            print('  /|\\')
+            print('  /' + ' ' + '\\')
+        elif wrong >= 4:
+            print('   x')
+            print('  /|\\')
+            print('  /' + ' ' + '\\')
+
 
 
