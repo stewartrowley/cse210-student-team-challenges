ccfe359e20cb2c84d2dfc8b657e19101139e593f
garrettbadger <garrett.badger.id@gmail.com>
2021-10-20 10:51:30 -0600

    created methods

---
+++ 06-mastermind/mastermind/game/board.py
@@ -0,0 +1,16 @@
+import random
+class Board:
+    def __init__(self):
+        pass
+
+    def _prepare(self):
+        pass
+
+    def apply(self, guess):
+        pass
+
+    def to_string(self):
+        pass
+
+    def did_win(self):
+        pass
