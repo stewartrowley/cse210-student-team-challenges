8c023243f5da9874372d12c9f35cfc677c2f00e6
Corey Yeaman <coreyyea@gmail.com>
2021-10-20 11:16:52 -0600

    update

--- 06-mastermind/mastermind/game/board.py
+++ 06-mastermind/mastermind/game/board.py
@@ -1,19 +1,18 @@
 import random
 class Board:
     def __init__(self):
-        self.guess = 0
-        self.code = 0
-        self._prepare()
-        
-        
+        pass
 
     def _prepare(self):
-        self.code = random.randint(1000, 9999)
+        pass
 
     def apply(self, guess):
-        self.guess = guess
+        pass
 
     def to_string(self):
+        text = "\n--------------------"
+        
+        text = "\n--------------------"
         pass
 
     def did_win(self):
