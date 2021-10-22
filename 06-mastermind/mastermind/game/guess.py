a5cf6b0bf29f394edd6c57657184814e68532411
Row84M <stewy.rowley@gmail.com>
2021-10-22 11:25:03 -0600

    update

--- 06-mastermind/mastermind/game/guess.py
+++ 06-mastermind/mastermind/game/guess.py
@@ -1,10 +1,18 @@
 class Guess:
     def __init__(self, code):
-        self.code = code
+        
+        self.code = []
+        for number in str(code):
+            self.code.append(int(number))
+
 
     def make_hint(self, guess):
-        get_hint = "****"
-        for index, letter in range(enumerate((guess))):
+        get_hint = ""
+        get_guess = []
+        for number in str(guess):
+            get_guess.append(int(number))
+ 
+        for index, letter in enumerate(get_guess):
             if self.code[index] == letter:
                 get_hint += "x"
             elif letter in self.code:
