dd6813e3fa1dd285016e8a3134bba0267041abaa
Row84M <stewy.rowley@gmail.com>
2021-11-01 11:16:06 -0600

    update

--- 07-speed/speed/game/constants.py
+++ 07-speed/speed/game/constants.py
@@ -4,5 +4,7 @@ MAX_X = 60
 MAX_Y = 20
 FRAME_LENGTH = 0.08
 STARTING_WORDS = 5
+DEFAULT_SQUARE_LENGTH = 20
+DEFAULT_FONT_SIZE = 16
 PATH = os.path.dirname(os.path.abspath(__file__))
 LIBRARY = open(PATH + "/words.txt").read().splitlines()
---
+++ 07-speed/speed/game/word.py
@@ -0,0 +1,26 @@
+import random
+from game import constants
+
+class Word(Actor):
+
+    def __init__(self):
+        super().__init__()
+        self._word_list = []
+        self._words = constants.LIBRARY
+        self._height = constants.DEFAULT_SQUARE_LENGTH
+        self._width  = constants.DEFAULT_SQUARE_LENGTH
+
+    def _word(self):
+        for i in self._words:
+            self._words.append[i]
+        
+
+        
+        
+    def _get_points(self):
+        return 
+
+    def _reset(self):
+        pass
+
+
