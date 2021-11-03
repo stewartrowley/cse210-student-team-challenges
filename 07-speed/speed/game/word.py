1df557992eaab672e2bac8e5ef2892000a2ae65a
Row84M <stewy.rowley@gmail.com>
2021-11-03 11:17:29 -0600

    update

--- 07-speed/speed/game/word.py
+++ 07-speed/speed/game/word.py
@@ -1,3 +1,4 @@
+from game.actor import Actor
 import random
 from game import constants
 
@@ -9,18 +10,21 @@ class Word(Actor):
         self._words = constants.LIBRARY
         self._height = constants.DEFAULT_SQUARE_LENGTH
         self._width  = constants.DEFAULT_SQUARE_LENGTH
+        self._points = 0
+
 
     def _word(self):
         for i in self._words:
-            self._words.append[i]
-        
+            self._words.append(self._word_list[i])
 
-        
-        
-    def _get_points(self):
-        return 
-
-    def _reset(self):
+    def _generate_word(self):
         pass
+        
+    def _get_points(self, word):
+        self._points = len(word)
+        return self._points
+
+    def _reset(self, word):
+        self._word_list.remove(word)
 
 
