b54a6c49ac56c5752df1ae61b4376a640a39684b
Row84M <stewy.rowley@gmail.com>
2021-11-05 11:51:42 -0600

    update

--- 07-speed/speed/__main__.py
+++ 07-speed/speed/__main__.py
@@ -1,6 +1,5 @@
 import os
 os.environ['RAYLIB_BIN_PATH'] = r'C:\Users\StewM\Documents\BYU Idaho\Fall 2021\programmingCourses\CSE210_personal\cse210-student-solo-checkpoints-1\07-snake\libraylib_shared.dll'
-
 from game.director import Director
 from game.input_service import InputService
 from game.output_services import OutputService
