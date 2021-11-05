3c67cf5e18c2a19428807bd9d73c8acffd187c35
Row84M <stewy.rowley@gmail.com>
2021-11-05 13:09:43 -0600

    update

--- 07-speed/speed/__main__.py
+++ 07-speed/speed/__main__.py
@@ -1,8 +1,7 @@
-import os
-os.environ['RAYLIB_BIN_PATH'] = r'C:\Users\StewM\Documents\BYU Idaho\Fall 2021\programmingCourses\CSE210_personal\cse210-student-solo-checkpoints-1\07-snake\libraylib_shared.dll'
-from game.director import Director
-from game.input_service import InputService
 from game.output_services import OutputService
+from game.input_service import InputService
+import os
+os.environ['RAYLIB_BIN_PATH'] = r'C:\Users\StewM\Documents\BYU Idaho\Fall 2021\programmingCourses\CSE210_team\cse210-student-team-challenges\07-speed\raylib-2.0.0-Win64-mingw\lib'
 
 
 def main():
@@ -10,6 +9,7 @@ def main():
     output_service = OutputService()
     director = Director(input_service, output_service)
     director.start_game()
+
 
 if __name__ == "__main__":
     main()
